e2f430476e492fa1b126ecb66104094d2e6cd573
jussinx <njorogee012@gmail.com>
2022-05-19 13:38:54 +0300

    strings in python

---
+++ calc.py
@@ -0,0 +1,7 @@
+f_num=float(input("enter the first number"))
+s_num=float(input("enter the second number"))
+sum_of_nums=f_num+s_num
+print(sum_of_nums)
+p_num=float(input("enter the first numer"))
+z_num=float(input("enter the first number"))
+print(sum_of_nums)
---
+++ strings.py
@@ -0,0 +1,34 @@
+name="emmanuel muriuki"
+
+#mltiple line strings
+msg="""QRS3X6 CONFIRMED You have recieved ksh 1200 from Janet Mueni"""
+print(msg)
+city="nairobi"
+print(city.upper())
+uni="JKUAT"
+print(uni.lower())
+fruit="apple"
+print(fruit[:2])
+print(fruit[-1])
+print(fruit[-3:])
+#strip is to remove space
+f_name="emmanuel"
+print(f_name.strip())
+#concatination
+s_name="muriuki"
+full_name = f_name+s_name
+print(full_name)
+city="juja"
+print(len(city))
+word="deoxyhaemoglobin"
+print(len(word))
+x=100
+print(str(x))
+y=3.14
+print(int(y))#int
+
+
+
+
+
+
