f6fb447cb3a2295f70d2ed57144b46ced4589513
jussinx <105852758+jussinx@users.noreply.github.com>
2022-05-27 08:44:20 +0300

    Add files via upload

---
+++ Gozo.py
@@ -0,0 +1,9 @@
+def replace_in(phrase) :
+    word=" "
+    for letter in phrase  :
+        if letter.lower () in "aeiou":
+            word = word + "g"
+        else :
+                word=word+letter
+                return word
+print(replace_in(input("Enter a phrase: ")))
