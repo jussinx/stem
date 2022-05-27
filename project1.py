f5a6f4d009594e9b63c25a1bfb703debca765878
jussinx <105852758+jussinx@users.noreply.github.com>
2022-05-27 08:46:23 +0300

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,16 @@
+
+price = 50
+price = 10
+price = 20
+print(price)
+banger = 666
+print(banger)
+print(price)
+price = 4.6
+print(price)
+first_name = "Mustafa "
+second_name = "Masasa "
+print(first_name + second_name)
+input(first_name)
+name = input("what is your name")
+print()
